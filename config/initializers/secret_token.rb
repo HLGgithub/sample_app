9d98711065a4978b70e286984ded42775712124c
HLGgithub <jumal@harvestlearninggroup.com>
2013-04-12 11:35:57 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '8cacf9f896c3eb80de8d58905555d36ba04350275837319ba11de1df6372b9b1a38f8d87e74bd70ff256d5ea1c0c9945b19d6ab1dd4e8bf4fc46a3fa39b8eccb'
