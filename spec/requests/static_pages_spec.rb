ddfdd12865db95d8c6d650cbd6b16e90513d18d8
HLGgithub <jumal@harvestlearninggroup.com>
2013-05-04 01:08:42 -0700

    contact page working

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -17,7 +17,7 @@ describe "Static pages" do
       it "should have the title 'Home' " do
         visit '/static_pages/home'
         page.should have_selector('title', 
-                                   :text=> "#{base_title}| Home")
+                                   :text=> "#{base_title} | Home")
       end
     end
 
@@ -31,7 +31,7 @@ describe "Static pages" do
 
       it "should have the title 'Help' " do
         visit '/static_pages/help'
-        page.should have_selector('title', :text => "#{base_title}| Help")
+        page.should have_selector('title', :text => "#{base_title} | Help")
       end
     end
 
@@ -45,7 +45,7 @@ describe "Static pages" do
 
       it "should have the title 'About Us' " do
         visit '/static_pages/about'
-        page.should have_selector('title', :text=> "#{base_title}| About Us")
+        page.should have_selector('title', :text=> "#{base_title} | About Us")
       end
     end
 
@@ -60,7 +60,7 @@ describe "Static pages" do
 
       it "should have the title 'Contact'" do
           visit '/static_pages/contact'
-          page.should have_selector('title', :text=>'Ruby on Rails Tutorial Sample App | Contact')
+          page.should have_selector('title', :text=> "#{base_title} | Contact")
       end
     end
 
