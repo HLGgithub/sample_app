6e2197154e94500e9715ee31c6764109b6f8bd74
HLGgithub <jumal@harvestlearninggroup.com>
2013-05-16 01:51:24 -0700

    About to add index

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,5 +12,15 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :email
 
-  validates :name, presence: true
+  validates :name, presence: true, length: {maximum:50}
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :email, presence: true, format: {with: VALID_EMAIL_REGEX},
+  uniqueness: {case_sensitive: false}
+
+
+
+
+
 end
