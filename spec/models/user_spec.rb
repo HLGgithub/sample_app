e4bb90a4722e9d443595f2621b61fba0279f0fef
HLGgithub <jumal@harvestlearninggroup.com>
2013-05-17 14:14:45 -0700

    Add user authentication and password encryption

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,32 +3,47 @@ require 'spec_helper'
 describe User do
 
   before do
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
   end
 
   subject { @user }
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
+  it { should respond_to(:password_digest)}
+  it { should respond_to(:password)}
+  it { should respond_to(:password_confirmation)}
+  it { should respond_to(:authenticate)}
 
   it { should be_valid }
 
-  describe "name is not present" do
+  describe "has a password that is blank" do
+    before { @user.password = @user.password_confirmation = " "}
+    it {should_not be_valid}
+  end
+
+  describe "has a password that is nil, most likey from console input, since this can't happen on the web.." do
+    before { @user.password_confirmation = nil}
+    it {should_not be_valid}
+  end
+
+  
+  describe "has a name that is not present" do
     before { @user.name = " " }
     it { should_not be_valid }
   end
 
-  describe "email is not present" do
+  describe "has an email that is not present" do
     	before { @user.email = " " }
     	it { should_not be_valid }
   end
 
-  describe "name is too long" do
+  describe "has a name that is too long" do
     	before { @user.name = "a" * 51 }
     	it { should_not be_valid }
   end
 
-   describe "email format is invalid" do
+   describe "has an email format that is invalid" do
     it "should be invalid" do
       addresses = %w[user@foo,com user_at_foo.org example.user@foo.
                      foo@bar_baz.com foo@bar+baz.com]
@@ -39,7 +54,7 @@ describe User do
     end
   end
 
-  describe "email format is valid" do
+  describe "has an email format that is valid" do
     it "should be valid" do
       addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
       addresses.each do |valid_address|
@@ -49,7 +64,7 @@ describe User do
     end
   end
 
-  describe "email address is already taken" do
+  describe "has an email address that has already been used" do
   	before do
   		user_with_same_email = @user.dup
   		user_with_same_email.email = @user.email.upcase
@@ -59,6 +74,45 @@ describe User do
   	it { should_not be_valid }
 
   end
+
+  describe "calls authenticate" do
+    before { @user.save }
+    let (:found_user) { User.find_by_email(@user.email) }
+
+    describe "and password is the user's real password" do
+      it {should == found_user.authenticate(@user.password)}
+    end
+
+    describe "and password is not the user's real password" do
+
+      let(:user_with_wrong_password) {found_user.authenticate("This_Is_An_Invalid_Password")}
+      
+      # The user should not be the same as the test user
+      it {should_not == user_with_wrong_password}
+
+      # Furthermore, no user should be found
+      specify {user_with_wrong_password.should be_false}
+    end
+
+  describe "has a password that is too short" do
+    before {@user.password = @user.password_confirmation = "a"*5}
+    it {should_not be_valid }
+  end
+
+
+
+  end
+
+
+
+
+
+
+
+
+
+
+
 
 
 end
